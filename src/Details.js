f921d85404791d6f6d3b42fe94174953feb689b6
nimaiwalsh <nimai.walsh@gmail.com>
2018-10-06 19:45:22 +1000

    reach router

---
+++ src/Details.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Details extends Component {
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    );
+  }
+}
+
+export default Details;
