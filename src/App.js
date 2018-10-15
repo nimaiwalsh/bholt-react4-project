416f116e14f923fff17e5754e4799eb0bbdb5eca
nimaiwalsh <nimai.walsh@gmail.com>
2018-10-16 09:16:33 +1000

    emotion

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Link } from '@reach/router'
+import { Router } from '@reach/router'
 import pf from 'petfinder-client'
 import { Provider } from './SearchContext'
 
+import NavBar from './NavBar'
 import Results from './Results'
 import Details from './Details'
 import SearchParams from './SearchParams'
@@ -72,14 +73,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <header>
-          <Link to="/">Adopt me!</Link>
-          <Link to='/search-params'>
-            <span aria-label="search" role="img">
-              🔍
-            </span>
-          </Link>
-        </header>
+        <NavBar />
         <Provider value={this.state}>
           <Router>
             <Results path="/" />
